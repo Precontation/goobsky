558365eeef05b3f49c5d86a5488a55553f8ff347
Maxwell Rubinstein <precontation@proton.me>
2026-08-24 21:00:10 -0700

    fix annoying bug

--- src/lib/components/post/Post.svelte
+++ src/lib/components/post/Post.svelte
@@ -45,7 +45,7 @@
 
 	<div class="flex gap-2">
 		<div
-			class="relative shrink-0 self-stretch {threadLine === 'both'
+			class="relative h-fit shrink-0 self-stretch {threadLine === 'both'
 				? 'thread-middle'
 				: threadLine === 'above'
 					? 'thread-start'
